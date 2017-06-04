6233b390e2f8655057242ce0db556486fc4add97
Homnibus <eseunes@gmail.com>
2017-06-04 18:37:43 +0200

    plateau + roi
    ajout plateau
    ajout roi
    ajout deplacement roi tour par tour

---
+++ alphashroom/Assets/scripts/Roi.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roi : MonoBehaviour {
+
+    public Transform terrainDeJeu;
+    public float deplacementRate = 0.25f;
+    private float attenteDeplacement;
+    private bool bouge;
+    private Vector3 direction;
+    private Vector3 prochainePosition;
+
+    // Use this for initialization
+    void Start () {
+        Vector3 positionInitiale = terrainDeJeu.gameObject.GetComponent<TerrainDeJeu>().donnerPositionDepart();
+        transform.position = positionInitiale;
+
+        attenteDeplacement = 0f;
+        bouge = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // 3 - Retrieve axis information
+        float inputY = Input.GetAxis("Horizontal");
+        float inputX = Input.GetAxis("Vertical");
+
+        if (peutBouger())
+        {
+            direction = Vector3.zero;
+            if (inputX > 0)
+            {
+                direction += Vector3.up;
+            }
+            else if (inputX < 0)
+            {
+                direction += Vector3.down;
+            }
+            if (inputY < 0)
+            {
+                direction += Vector3.left;
+            }
+            else if (inputY > 0)
+            {
+                direction += Vector3.right;
+            }
+
+            if (!direction.Equals(Vector3.zero))
+            {
+                bouge = true;
+                attenteDeplacement += deplacementRate;
+                prochainePosition = transform.position + direction;
+            }
+        }
+
+        if (bouge)
+        {
+            Vector3 mouvement = (direction * (Time.deltaTime)) / deplacementRate;
+            transform.position += mouvement;
+
+            if (attenteDeplacement <= 0f)
+            {
+                transform.position = prochainePosition;
+                bouge = false;
+            }
+            else
+            {
+                attenteDeplacement -= Time.deltaTime;
+            }
+
+        }
+
+    }
+
+    private bool peutBouger()
+    {
+        return attenteDeplacement <= 0f && !bouge;
+    }
+
+}
---
+++ alphashroom/Assets/scripts/TerrainDeJeu.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainDeJeu : MonoBehaviour {
+
+    // Tableau de cellules
+    private Transform[,] cells;
+    private int[,] intTab =
+        {
+            {1,2,3},
+            {4,5,6},
+            {7,0,9},
+        }
+    ;
+
+    //Position de depart
+    private Vector3 positionDepart;
+
+    //Type de cellules
+    public Transform grassCell;
+
+    // Use this for initialization
+    void Awake() {
+        cells = intTabToCells(intTab);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private Transform[,] intTabToCells(int[,] intTab) {
+        Transform[,] cells = new Transform[intTab.GetLength(0), intTab.GetLength(1)];
+        Vector3 tailleCase = (grassCell.GetComponent<SpriteRenderer>().bounds.size);
+
+
+        for (int i = 0; i < intTab.GetLength(0); i++)
+        {
+            for (int j = 0; j < intTab.GetLength(1); j++)
+            {
+                cells[i, j] = Instantiate(grassCell) as Transform;
+                Debug.Log("Cellule x : " + i + "y : " + j + " valeur : " + intTab[i, j]);
+                Debug.Log("test x : " + tailleCase.x * (i + 1) + " y : " + tailleCase.y * (j + 1) + " z : " + tailleCase.z);
+                cells[i, j].position = new Vector3(tailleCase.x * i, tailleCase.y * j, tailleCase.z);
+                if (intTab[i, j] == 0)
+                {
+                    positionDepart = new Vector3(tailleCase.x * i, tailleCase.y * j, tailleCase.z);
+                }
+            }
+        }
+
+
+        return cells;
+    }
+
+    public Vector3 donnerPositionDepart()
+    {
+        return positionDepart;   
+    }
+
+}
