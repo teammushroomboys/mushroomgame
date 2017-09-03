8a6defe7fa0598469be2b8fd424416e1f16a41c5
Homnibus <eseunes@gmail.com>
2017-09-03 16:11:44 +0200

    ajout animation roi

--- alphashroom/Assets/scripts/Roi.cs
+++ alphashroom/Assets/scripts/Roi.cs
@@ -6,11 +6,13 @@ public class Roi : MonoBehaviour {
 
     public Transform terrainDeJeu;
     public Vector2 speed = new Vector2(50, 50);
+    private Animator animator;
 
     // Use this for initialization
     void Start () {
         Vector3 positionInitiale = terrainDeJeu.gameObject.GetComponent<TerrainDeJeu>().donnerPositionDepart();
         transform.position = positionInitiale;
+        animator = this.GetComponent<Animator>();
     }
 	
 	// Update is called once per frame
@@ -23,6 +25,27 @@ public class Roi : MonoBehaviour {
           speed.x * inputX,
           speed.y * inputY);
         transform.position += new Vector3(movement.x,movement.y,0);
+        animator.speed = 1;
+        if (inputX > 0)
+        {
+            animator.SetInteger("Direction", 2);
+        }
+        else if (inputX < 0)
+        {
+            animator.SetInteger("Direction", 1);
+        }
+        else if (inputY > 0)
+        {
+            animator.SetInteger("Direction", 0);
+        }
+        else if (inputY < 0)
+        {
+            animator.SetInteger("Direction", 3);
+        }
+        else
+        {
+            animator.speed = 0;
+        }
 
     }
 }
